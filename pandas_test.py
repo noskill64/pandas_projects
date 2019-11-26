680b5c0cb704e60acd5aab53d0b3f1536784f579
MacBook <franky.nelis64@gmail.com>
2019-11-26 10:15:06 +0100

    Intital commit

--- pandas_test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-# from openpyxl.workbook import workbook
-
-
-my_sheet = 'Settings'
-file_name = '/Users/Admin/Desktop/import_testcases/Reviewed Test cases_DCD_Updated_19062018.xlsx'  # name of your excel file
-df = pd.read_excel(file_name, sheet_name=my_sheet)
-# print(df)
-print(df.head())
